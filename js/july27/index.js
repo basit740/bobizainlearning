0d863abc31f9ebd44d7db5ee7606831339a4ce5d
Basit Ali <alib4111@gmail.com>
2022-07-28 00:17:03 +0500

    pushed again

--- js/july27/index.js
+++ js/july27/index.js
@@ -7,3 +7,20 @@ function testAnd(num) {
 
 const result = testAnd(4);
 console.log(result);
+
+////////////////////////// Assignmment for AND operator   //////////////////////////
+
+function testLogicalAnd(val) {
+	// Only change code below this line
+
+	if (val) {
+		if (val) {
+			return 'Yes';
+		}
+	}
+
+	// Only change code above this line
+	return 'No';
+}
+
+testLogicalAnd(10);
