e515cf297eba6b351d2fbb5aa4d141f46dae01b0
Basit Ali <alib4111@gmail.com>
2022-07-28 00:12:19 +0500

    july25 js lecture done

--- js/july27/index.js
+++ js/july27/index.js
@@ -1,1 +1,9 @@
-console.log('welcome to coding again in july');
+function testAnd(num) {
+	if (num > 5 && num < 10) {
+		return 'Yes';
+	}
+	return 'No';
+}
+
+const result = testAnd(4);
+console.log(result);
